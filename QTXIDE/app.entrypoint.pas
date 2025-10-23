4debd358ef0a20aea296a4ef5fed9dc16adee153
LynkFS <lynkfs@gmail.com>
2025-10-23 17:10:23 +1000

    Update app.entrypoint.pas

--- QTXIDE/app.entrypoint.pas
+++ QTXIDE/app.entrypoint.pas
@@ -2,9 +2,10 @@
 
 //create forms
 Application.CreateForm('Form1',TForm1);
-Application.CreateForm('Form2',TForm2);
-Application.CreateForm('Form3',TForm3);
-Application.CreateForm('Form4',TForm4);
+//Application.CreateForm('Form2',TForm2);
+//Application.CreateForm('Form3',TForm3);
+//Application.CreateForm('Form4',TForm4);
 
 //show initial form
 Application.GoToForm('Form1');
+
